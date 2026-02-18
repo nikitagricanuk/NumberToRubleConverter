4eea672136abdd047eede2cdec9afde5bf94572b
Nikita Gritsanyuk <doting.laws0d@icloud.com>
2026-02-18 12:09:58 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace NumberToRubleConverter;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int kopeks = int.Parse(Console.ReadLine());
+        if (kopeks % 100 == 0)
+        {
+            
+        }
+        Console.WriteLine(kopeks);
+    }
+}
