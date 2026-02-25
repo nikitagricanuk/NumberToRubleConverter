ed38ef8b20111ca5feecb1b4ad30656d73bb32de
Nikita Gritsanyuk <doting.laws0d@icloud.com>
2026-02-25 10:42:34 +0800

    Added main algorithm

--- Program.cs
+++ Program.cs
@@ -5,10 +5,30 @@ class Program
     static void Main(string[] args)
     {
         int kopeks = int.Parse(Console.ReadLine());
-        if (kopeks % 100 == 0)
+        int rublesFinal = 0;
+        int kopeksFinal;
+        if (kopeks >= 100 && kopeks <= 200) // 100 <= kopeks <= 200
         {
-            
+            rublesFinal += 1;
+            kopeks -= 100;
+            kopeksFinal = kopeks;
         }
-        Console.WriteLine(kopeks);
+        else if (kopeks >= 200 && kopeks <= 300)
+        {
+            rublesFinal += 2;
+            kopeks -= 200;
+            kopeksFinal = kopeks;
+        }
+        else if (kopeks >= 300 && kopeks <= 400)
+        {
+            rublesFinal += 3;
+            kopeks -= 300;
+            kopeksFinal = kopeks;
+        }
+        else
+        {
+            kopeksFinal = kopeks;
+        }
+        Console.WriteLine(rublesFinal + " рублей " + kopeksFinal + " копеек");
     }
 }
