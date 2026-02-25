c9251339a67139583289194070f3bcd5453b5edd
Nikita Gritsanyuk <doting.laws0d@icloud.com>
2026-02-25 10:43:19 +0800

    Extracted GetKopeks and GetRubles

--- Program.cs
+++ Program.cs
@@ -32,3 +32,41 @@ class Program
         Console.WriteLine(rublesFinal + " рублей " + kopeksFinal + " копеек");
     }
 }
+
+public class Converter
+{
+    public static int GetRubles(int kopeks)
+    {
+        int rublesFinal = 0;
+        if (kopeks >= 100 && kopeks <= 200) // 100 <= kopeks <= 200
+        {
+            rublesFinal += 1;
+            kopeks -= 100;
+        }
+        else if (kopeks >= 200 && kopeks <= 300)
+        {
+            rublesFinal += 2;
+            kopeks -= 200;
+        }
+        else if (kopeks >= 300 && kopeks <= 400)
+        {
+            rublesFinal += 3;
+            kopeks -= 300;
+        }
+
+        return rublesFinal;
+    }
+    public static int GetKopeks(int number)
+    {
+        if (number >= 1000)
+        {
+            number -= 1000;
+        }
+
+        if (number >= 100)
+        {
+            number -= 100;
+        }
+        return number;
+    }
+}
